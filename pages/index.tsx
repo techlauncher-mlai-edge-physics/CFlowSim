920acb944e7c99f53eba7c1c72d1c715720ef00d
Bill ZHANG <36790218+Lutra-Fs@users.noreply.github.com>
2023-04-18 12:57:35 +1000

    refactor(index): remove the unused imports and variables

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import styles from "styles/Home.module.css";
 import NavBar from "../Components/NavBar";
-import { Button } from "antd";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
