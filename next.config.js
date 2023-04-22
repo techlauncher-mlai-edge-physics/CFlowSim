1283150868aab2f440e536901b8ee844dc2d2703
Bill ZHANG <36790218+Lutra-Fs@users.noreply.github.com>
2023-04-22 21:12:55 +1000

    refactor(PhysicsCore): refactor PhysicsCore to modelService with TS

--- next.config.js
+++ next.config.js
@@ -1,10 +1,36 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
+const CopyPlugin = require("copy-webpack-plugin");
+
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
   images: {
     unoptimized: true,
   },
+  webpack(config, options) {
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
+          {
+            from: "./node_modules/onnxruntime-web/dist/ort-wasm.wasm",
+            to: "static/chunks/pages",
+          },
+          {
+            from: "./node_modules/onnxruntime-web/dist/ort-wasm-simd.wasm",
+            to: "static/chunks/pages",
+          },
+          {
+            from: "./model",
+            to: "static/chunks/pages/model",
+          },
+        ],
+      })
+    );
+    return config;
+  },
+  env: {
+    BASE_PATH: process.env.NODE_ENV === "production" ? "": "http://localhost:3000",
+  }
 };
 
 module.exports = nextConfig;
