98a00286693c0de43737d11760dc692f9ee9eef2
Bill ZHANG <36790218+Lutra-Fs@users.noreply.github.com>
2023-05-09 01:11:16 +1000

    feat(index): init simulator integration

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,34 @@ import Head from "next/head";
 import styles from "styles/Home.module.css";
 import NavBar from "@components/NavBar";
 import ParBar from "@components/ParametersBar";
+import { Canvas } from "@react-three/fiber";
+import styled from "styled-components";
+import { DiffusionPlane, SimulationParams } from "@components/Simulation";
+import { useEffect, useState } from "react";
 
+const Simulator = styled.div`
+  position: absolute;
+  left: 21rem;
+  top: 90px;
+  width: calc(100% - 22rem);
+  height: calc(100% - 100px);
+`;
 
 export default function Home(): React.ReactElement {
+  const params: SimulationParams = new SimulationParams();
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const [worker, setWorker] = useState<Worker>(null!);
+
+  useEffect(() => {
+    const worker = new Worker(
+      new URL("../workers/modelWorker", import.meta.url),
+      {
+        type: "module",
+      }
+    );
+    setWorker(worker);
+  }, []);
+
   return (
     <>
       <Head>
@@ -17,7 +42,23 @@ export default function Home(): React.ReactElement {
         <div className={styles.navBar}>
           <NavBar />
         </div>
-          <ParBar />
+        <ParBar />
+        <Simulator>
+          <Canvas
+            shadows
+            className={styles.canvas}
+            camera={{
+              position: [1, 10, 1],
+            }}
+          >
+            <ambientLight />
+            <DiffusionPlane
+              position={[0, 0, 0]}
+              params={params}
+              worker={worker}
+            />
+          </Canvas>
+        </Simulator>
       </main>
     </>
   );
