5b3f0a7b1fb19ed2df0e6cf28cf22d62c9faac25
Bill ZHANG <36790218+Lutra-Fs@users.noreply.github.com>
2023-05-09 13:49:50 +1000

    feat(about): add content for about page (#12)
    * feat(about): render markdown as about page content
    
    use markdown to render is easy to maintain now. would probably refactor
    later
    
    * fix(about): update about page's code
    
    this commit is a follow up with 13a3592d26404acd6ef3833f552c1a10e577b787
    as it miss the about.tsx code
    
    * docs(about): add contents
    
    * docs(about): remove inline HTML and reset the heading order
    
    * docs(about): fix typo for single quotation mark for ReactMarkdown
    
    ---------
    
    Co-authored-by: Shuyuan He <u7355023@anu.edu.au>

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,11 +1,27 @@
 import NavBar from "@components/NavBar";
 import styles from "styles/Home.module.css";
+import Head from "next/head";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import aboutContent from "../docs/about.md";
+import remarkGfm from "remark-gfm";
 
-export default function AboutPage(): JSX.Element {
-    return (
+export default function AboutPage(): React.ReactElement {
+  return (
+    <>
+      <Head>
+        <title>Physics in the Browser for the People</title>
+        <meta name="description" content="PHYS" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
         <div className={styles.navBar}>
-            <NavBar />
+          <NavBar />
         </div>
-    );
+        <div className={styles.about}>
+          <ReactMarkdown remarkPlugins={[remarkGfm]}>{aboutContent}</ReactMarkdown>
+        </div>
+      </main>
+    </>
+  );
 }
-
