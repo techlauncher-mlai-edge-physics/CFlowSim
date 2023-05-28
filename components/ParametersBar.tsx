c9b4a023113a42f615e7e0edb61c992d6f47eb70
Bill ZHANG <36790218+Lutra-Fs@users.noreply.github.com>
2023-05-29 00:19:16 +1000

    feat(ParametersBar): add options to change colors (#17)
    * feat(ParametersBar): add options to change colors
    
    - Bump antd to 5.5.0 to introduce color picker
    - use color picker to implement changes for 2 color value
    
    * refactor(ParametersBar): use antd to render
    
    - use antd to render
    - add styling for Color Picker
    
    Signed-off-by: Bill ZHANG <36790218+Lutra-Fs@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Bill ZHANG <36790218+Lutra-Fs@users.noreply.github.com>

--- components/ParametersBar.tsx
+++ components/ParametersBar.tsx
@@ -1,6 +1,12 @@
 import styled from "styled-components";
+import { ColorPicker, Divider, Col, Space, Row } from "antd";
+import { useEffect, useMemo, useState } from "react";
+import type { Color } from "antd/es/color-picker";
+import { Color as ThreeColor } from "three";
+import { type SimulationParams } from "./Simulation";
+import { type SpaceSize } from "antd/es/space";
 
-const Container = styled.div`
+const Container = styled(Space)`
   background-color: #d3d3d3;
   color: #000000;
   width: 20rem;
@@ -9,19 +15,66 @@ const Container = styled.div`
   font-size: 2rem;
   min-height: 90%;
   position: absolute;
+  display: flex;
 `;
-const Title = styled.div`
+const Title = styled.span`
   font-family: "Roboto", sans-serif;
   font-size: 1.4rem;
-  position: absolute;
-  left: 5.5rem;
-  top: 1rem;
 `;
 
-export default function ParametersBar(): React.ReactElement {
+export default function ParametersBar(props: {
+  params: SimulationParams;
+  setParams: React.Dispatch<React.SetStateAction<SimulationParams>>;
+}): React.ReactElement {
+  const [colorLow, setColorLow] = useState<Color | string>("#0000FF");
+  const [colorHigh, setColorHigh] = useState<Color | string>("#FF0000");
+
+  const setParams = props.setParams;
+
+  const colorLowString = useMemo(
+    () => (typeof colorLow === "string" ? colorLow : colorLow.toHexString()),
+    [colorLow]
+  );
+  const colorHighString = useMemo(
+    () => (typeof colorHigh === "string" ? colorHigh : colorHigh.toHexString()),
+    [colorHigh]
+  );
+
+  useEffect(() => {
+    setParams((prev) => {
+      return {
+        ...prev,
+        densityLowColour: new ThreeColor(colorLowString),
+        densityHighColour: new ThreeColor(colorHighString),
+      };
+    });
+  }, [colorLowString, colorHighString, setParams]);
+  const space: [SpaceSize, SpaceSize] = ["large", "small"];
+
   return (
-    <Container>
-      <Title>Parameters</Title>
+    <Container direction="vertical" size={space}>
+      <Row justify="center"/>
+      <Row justify="center">
+        <Title>Parameters</Title>  
+      </Row>
+      <Divider orientation="left">Simulator Color</Divider>
+      <Row justify="space-around">
+        <Col style={{ textAlign: "center" }}>
+          <span>Low</span>
+        </Col>
+        <Col>
+          <ColorPicker value={colorLow} onChange={setColorLow} />
+        </Col>
+      </Row>
+      <Row justify="space-around">
+        <Col>
+          <span>High</span>
+        </Col>
+        <Col>
+          <ColorPicker value={colorHigh} onChange={setColorHigh} />
+        </Col>
+      </Row>
+      <Divider />
     </Container>
   );
 }
