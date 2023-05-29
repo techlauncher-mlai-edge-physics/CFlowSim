318e5563d3317fa3c4852e824b044ba04d77d22a
Chenzhu <u7171958@anu.edu.au>
2023-05-29 17:38:23 +1000

    feat(simulator): convert rendering to texture

--- shaders/vert.glsl
+++ shaders/vert.glsl
@@ -1,29 +1,26 @@
 // VERTEX SHADER
 varying lowp vec4 vColor;
 
-uniform float density[${segArea}]; //the size of one chunk 
-uniform vec3 hiCol; 
+uniform sampler2D density;
+uniform vec3 hiCol;
 uniform vec3 lowCol;
-
-int getIndexFromPoint(vec3 pos)
-{
-  int ix = int((pos.x + (${width} / 2.0)) * ${segX} / ${width});
-  int iy = int((-pos.y + (${height} / 2.0)) * ${segY} / ${height});
-  return (ix + iy * ${segXInt});
-}
 
 vec4 getColourFromDensity(float density)
 {
-  density = min(density, ${densityRangeHigh});
-  density = max(density, ${densityRangeLow});
-  density = density / ${densityRangeSize};
   return vec4(mix(lowCol, hiCol, density), 1.0);
 }
 
-void main(void) 
+vec2 getCoordFromPoint(vec3 pos)
+{
+  float x = ((pos.x + (${width} / 2.0)) / ${width});
+  float y = ((-pos.y + (${height} / 2.0)) / ${height});
+  return vec2(x, y);
+}
+
+void main(void)
 {
   gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
 
-  int index = getIndexFromPoint(position);
-  vColor = getColourFromDensity(density[index]);
+  highp vec2 vTextureCoord = getCoordFromPoint(position);
+  vColor = getColourFromDensity(texture2D(density, vTextureCoord).r);
 }
