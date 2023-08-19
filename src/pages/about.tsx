2f051d0cd62ef9784565b1879cf1b60fffeb203d
Justin <mentairicebowl@gmail.com>
2023-08-20 00:37:21 +1000

    feat(about) added some styling to about page and read from `README.md`

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,6 +1,35 @@
-import aboutContent from '../../docs/about.md';
+import aboutContent from '../../README.md';
+import styled from 'styled-components';
+
+const Content = styled.div`
+  text-align:left;
+  margin-left: 25%;
+  margin-right: 25%;
+  margin-top: 4%;
+  margin-bottom: 8%;
+  font-family: 'Roboto', sans-serif;
+  a:link {
+    text-decoration: none;
+  }
+  a:visited {
+    text-decoration: none;
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+  a:active {
+    text-decoration: underline;
+  }
+`;
 
 export default function AboutPage(): React.ReactElement {
+  return (
+    <>
+      <Content>
+        <div dangerouslySetInnerHTML={{ __html: aboutContent as string }} />
+      </Content>
+    </>
+  );
   // return aboutContent as React.ReactElement
-  return <div dangerouslySetInnerHTML={{ __html: aboutContent as string }} />;
+  // return <div dangerouslySetInnerHTML={{ __html: aboutContent as string }} />;
 }
