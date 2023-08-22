dcbe0ca60640060989e938d2897807fb84b5c19d
Justin Tieu <u7280302@anu.edu.au>
2023-08-22 18:00:05 +1000

    feat(about): made text colour responsive to theme

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,12 +2,13 @@ import aboutContent from '../../README.md';
 import styled from 'styled-components';
 
 const Content = styled.div`
-  text-align:left;
+  text-align:justify;
   margin-left: 25%;
   margin-right: 25%;
   margin-top: 4%;
   margin-bottom: 8%;
   font-family: 'Roboto', sans-serif;
+  color: ${(props) => (props.theme.light ? '#333333' : '#c9c9c9' )};
   a:link {
     text-decoration: none;
   }
