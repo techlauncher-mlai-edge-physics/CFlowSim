2c1a246be593520c270238c4807e929aaea71936
esterTion <esterTion@users.noreply.github.com>
2023-09-07 20:45:24 +1000

    feat(parameter-pane): create components for parameter pane (#52)
    * feat(parameter-pane): create components for parameter pane
    
    * feat(parameter-pane): replace antd components with custom styled components
    
    - also reindent using space
    
    * fix(parameter-pane): fix styling
    
    ---------
    
    Co-authored-by: Chenzhu <u7171958@anu.edu.au>

---
+++ src/components/ParameterComponents/DropdownParameter.tsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+import { useState } from "react";
+
+const Select = styled.select`
+  -webkit-appearance: none;
+  appearance: none;
+  width: 15rem;
+  display: block;
+  border: black 1px solid;
+  background: rgb(217,217,217);
+  margin: 5px 0;
+  padding: 0 5px;
+  height: 24px;
+`
+
+export default function DropdownParameter(props: {
+  onChange: (value: string)=> void,
+  values: string[],
+  initValue?: string,
+}): React.ReactElement {
+
+  const [value, setValue] = useState(props.values[0])
+  if (props.initValue) {
+    setValue(props.initValue)
+  }
+  return (
+  <Select value={value} onChange={e => (setValue(e.target.value), props.onChange(e.target.value))}>
+    {props.values.map((val, i) => (
+      <option key={i} value={val} >{val}</option>
+    ))}
+  </Select>
+  );
+}
---
+++ src/components/ParameterComponents/ParameterLabel.tsx
@@ -0,0 +1,15 @@
+import { QuestionCircleOutlined } from "@ant-design/icons";
+import { Tooltip } from "antd"
+
+
+export default function ParameterLabel(props: {
+  title: string,
+  tooltip?: string,
+}): React.ReactElement {
+  const tooltip: React.ReactElement[] = []
+  if (props.tooltip) {
+    tooltip.push(<Tooltip placement="right" title={props.tooltip}><QuestionCircleOutlined /></Tooltip>)
+  }
+  const label = (<div>{props.title}&nbsp;&nbsp;{tooltip}</div>)
+  return label;
+}
