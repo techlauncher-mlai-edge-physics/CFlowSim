4dc8ebe985183ed6654fe2b7da6c27902c622a47
Chenzhu <u7171958@anu.edu.au>
2023-09-11 22:33:32 +1000

    feat(webgpu-rendering-context): import webgpu renderer from three.js and creates a renderer

--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -6,3 +6,15 @@ declare module '*.md' {
   const html: string;
   export default html;
 }
+
+declare module 'three/addons/capabilities/WebGPU.js' {
+  export class WebGPU {
+    static isAvailable(): boolean;
+    static getErrorMessage(): HTMLDivElement;
+  }
+}
+declare module 'three/addons/renderers/webgpu/WebGPURenderer.js' {
+  export class WebGPURenderer {
+    constructor( parameters: map<string, object> );
+  }
+}
