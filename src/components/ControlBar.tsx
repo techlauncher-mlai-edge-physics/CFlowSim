71cc2d23751a170259f89b4bb7946a58ceda5f50
Jingqi <lorenzovallovesnz@icloud.com>
2023-09-16 12:25:13 +1000

    feat(Save/Restore ui): add buttons to save or restore the model manually

--- src/components/ControlBar.tsx
+++ src/components/ControlBar.tsx
@@ -9,6 +9,36 @@ export const ControlBarContainer = styled(Space)`
   display: flex;
 `;
 
+export const SaveBtn = styled(Button)`
+  position: absolute;
+  bottom: 4rem;
+  right: 11rem;
+  color: #eeeeee;
+  background-color: #555555;
+  height: 3.2rem;
+  width: 8rem;
+  margin: 0 0.2rem;
+  cursor: pointer;
+  @media (max-width: 760px) {
+    margin-bottom: 0.2rem;
+  }
+`;
+
+export const RestoreBtn = styled(Button)`
+  position: absolute;
+  bottom: 4rem;
+  right: 2rem;
+  color: #eeeeee;
+  background-color: #555555;
+  height: 3.2rem;
+  width: 8rem;
+  margin: 0 0.2rem;
+  cursor: pointer;
+  @media (max-width: 760px) {
+    margin-bottom: 0.2rem;
+  }
+`;
+
 interface ControlBarProps {
   worker: Worker;
 }
@@ -16,35 +46,51 @@ interface ControlBarProps {
 export default function ControlBar(props: ControlBarProps): React.ReactElement {
   const { worker } = props;
   return (
-    <ControlBarContainer size="small" direction="horizontal">
-      <Button
+    <>
+      <SaveBtn
         onClick={() => {
-          worker.postMessage({ func: 'start' });
+          console.log('Click Save');
         }}
       >
-        Play
-      </Button>
-      <Button
+        Save Model
+      </SaveBtn>
+      <RestoreBtn
         onClick={() => {
-          worker.postMessage({ func: 'pause' });
+          console.log('Click Restore');
         }}
       >
-        Pause
-      </Button>
-      <Button
-        onClick={() => {
-          worker.postMessage({ func: 'stop' });
-        }}
-      >
-        Stop
-      </Button>
-      <Button
-        onClick={() => {
-          worker.terminate();
-        }}
-      >
-        TERMINATE
-      </Button>
-    </ControlBarContainer>
+        Restore Model
+      </RestoreBtn>
+      <ControlBarContainer size="small" direction="horizontal">
+        <Button
+          onClick={() => {
+            worker.postMessage({ func: 'start' });
+          }}
+        >
+          Play
+        </Button>
+        <Button
+          onClick={() => {
+            worker.postMessage({ func: 'pause' });
+          }}
+        >
+          Pause
+        </Button>
+        <Button
+          onClick={() => {
+            worker.postMessage({ func: 'stop' });
+          }}
+        >
+          Stop
+        </Button>
+        <Button
+          onClick={() => {
+            worker.terminate();
+          }}
+        >
+          TERMINATE
+        </Button>
+      </ControlBarContainer>
+    </>
   );
 }
