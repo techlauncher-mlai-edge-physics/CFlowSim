d5708eda1e9660e021791bd48847358c8d42cdd3
Bill ZHANG <36790218+Lutra-Fs@users.noreply.github.com>
2023-10-07 19:05:12 +1100

    feat(autosave;restore-ui): restore functionality from IndexedDB; restore ui (#162)
    * feat(restoreUI): add initial implemtation for restore ui
    
    Signed-off-by: Bill ZHANG <36790218+Lutra-Fs@users.noreply.github.com>
    
    * feat(autosave;restore-ui): restore functionality from IndexedDB
    
    Implemented a function that allows users to restore their model progress
    from the indexedDB. To do this, I've adjusted the ControlBar component
    to add a "Restore Popup" button that enables users to choose the key
    from the DB to restore. Additionally, the IndexedDBRestore functionality
    has been revamped for completeness and effectiveness. `Deserialize`
    function has been updated in the worker to parse the stringified
    ModelSave correctly, facilitating the restoration process.
    
    ---------
    
    Signed-off-by: Bill ZHANG <36790218+Lutra-Fs@users.noreply.github.com>

---
+++ src/components/RestoreComponents/RestorePopUp.tsx
@@ -0,0 +1,131 @@
+import { Divider, Menu } from 'antd';
+import type React from 'react';
+import { useState } from 'react';
+import styled from 'styled-components';
+import LocalFileRestore from './LocalFileRestore';
+import { type RestoreProps } from './RestoreProps';
+import IndexedDBRestore from './IndexedDBRestore';
+import { CloseOutlined } from '@ant-design/icons';
+
+const Popup = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 60%;
+  height: 80%;
+  background-color: #ffffff;
+  border-radius: 1rem;
+  transform: translate(-50%, -50%);
+  z-index: 100;
+
+  @media (max-width: 760px) {
+    width: 95%;
+    height: 97%;
+  }
+`;
+
+const IconButton = styled.button`
+  position: absolute;
+  top: 0.5rem;
+  right: 0.5rem;
+  background-color: transparent;
+  border: none;
+  font-size: 1rem;
+  cursor: pointer;
+`;
+
+const LeftColumn = styled.div`
+  width: 25%;
+  height: 100%;
+  float: left;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media (max-width: 760px) {
+    width: 95%;
+    float: none;
+    height: 30%;
+  }
+`;
+const SafeArea = styled.div`
+  width: 80%;
+  height: 90%;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+`;
+
+const RightColumn = styled.div`
+  width: 70%;
+  height: 100%;
+  float: left;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media (max-width: 760px) {
+    width: 100%;
+    float: none;
+    height: 70%;
+  }
+`;
+
+const MenuContainer = styled(Menu)`
+  width: 100%;
+  height: 100%;
+  border: none;
+`;
+
+export default function RestorePopup(props: RestoreProps): React.ReactElement {
+  const [selectedItem, setSelectedItem] = useState('');
+
+  const handleItemClick = (item: {
+    key: React.SetStateAction<string>;
+  }): void => {
+    setSelectedItem(item.key);
+  };
+
+  const renderRightColumn = (): React.ReactNode => {
+    switch (selectedItem) {
+      case 'A':
+        return <LocalFileRestore {...props} />;
+      case 'B':
+        return <IndexedDBRestore {...props} />;
+      default:
+        return null;
+    }
+  };
+
+  const handleCloseClick = (): void => {
+    // handle close button click here
+    // remove the popup from the DOM
+    const popup = document.getElementById('restore-popup');
+    if (popup !== null) {
+      popup.remove();
+    }
+  };
+
+  return (
+    <Popup id="restore-popup">
+      <IconButton onClick={handleCloseClick}>
+        <CloseOutlined />
+      </IconButton>
+      <LeftColumn>
+        <SafeArea>
+          <MenuContainer
+            onClick={handleItemClick}
+            selectedKeys={[selectedItem]}
+          >
+            <Menu.Item key="A">Local PC</Menu.Item>
+            <Menu.Item key="B">IndexedDB</Menu.Item>
+          </MenuContainer>
+        </SafeArea>
+      </LeftColumn>
+      <Divider type="vertical" />
+      <RightColumn>
+        <SafeArea>{renderRightColumn()}</SafeArea>
+      </RightColumn>
+    </Popup>
+  );
+}
